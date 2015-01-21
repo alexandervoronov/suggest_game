c70a061f410804239bde1da1b3f9d42d7d3f39c9
Alexander Voronov <ru.xalba@gmail.com>
2015-01-22 01:41:22 +0300

    Add batch processing from json.

--- suggest_game.py
+++ suggest_game.py
@@ -3,6 +3,8 @@
 import requests
 import argparse
 import markdown
+import os
+import json
 
 suggest_url = 'http://google.com/complete/search?client=firefox&output=json&ie=UTF-8&q={}'
 
@@ -12,6 +14,9 @@ def parse_args():
                         help='Request string.')
     parser.add_argument('--partial', dest='partial', default=False, action='store_true',
                         help='Don\'t interpret last word as a whole one')
+    parser.add_argument('-b', '--batch', dest='batch', default=None,
+                        help='JSON file with batch request.')
+    
     return parser.parse_args()
 
 def retrieve_suggests(request, partial=False):
@@ -25,15 +30,29 @@ def convert_to_markdown(request, answers):
     markdown_string = request + '\n'
     markdown_string += '=' * len(request) + '\n'
     markdown_string += '* ' + '\n* '.join(answers) + '\n'
+    markdown_string += '\n'
     return markdown_string
 
-
+def json_batch_suggests(json_path, partial=False):
+    assert os.path.isfile(json_path)
+    requests = []
+    md = ''
+    with open(json_path, encoding='utf8') as f:
+        requests = json.load(f)
+    for single_request in requests:
+        ans = retrieve_suggests([single_request], partial)
+        req_md = convert_to_markdown(single_request, ans)
+        md += req_md
+    return md
 
 if __name__ == '__main__':
     args = parse_args()
-    answers = retrieve_suggests(args.request, args.partial)
+    md = ''
+    if args.batch:
+        md = json_batch_suggests(args.batch, args.partial)
+    elif args.request:
+        answers = retrieve_suggests(args.request, args.partial)
+        md = convert_to_markdown(' '.join(args.request), answers)
 
-    # print('\n'.join(answers))
-    print (convert_to_markdown(' '.join(args.request), answers))
+    print(md)
     # print(markdown.markdown(convert_to_markdown(' '.join(args.request), answers))) # html
-    
